d26876a305ad4e551eede42299747fda3b32e856
Yuliia001 <babeyuliia@gmail.com>
2023-07-04 22:11:52 +0200

    add task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input[name="email"]'),
+    textarea: document.querySelector('textarea[name="message"]')
+}
+const { form, input, textarea } = refs;
+const LS_KEY = 'feedback-form-state';
+
+
+const data = {};
+
+function handlerGetInput() {
+    const savedInput = JSON.parse(localStorage.getItem(LS_KEY));
+    // console.log(savedInput);
+    if (savedInput) {
+        input.value = savedInput.email;
+        textarea.value = savedInput.message;
+    } 
+}
+handlerGetInput();
+
+form.addEventListener('input', throttle(handlerSetInput, 500));
+function handlerSetInput(evt) {
+    data[evt.target.name] = evt.target.value;
+    localStorage.setItem(LS_KEY, JSON.stringify(data));
+}
+
+form.addEventListener('submit', handlerClear);
+function handlerClear(evt) {
+    evt.preventDefault();
+    if (input.value && textarea.value) {
+        console.log(data);
+        form.reset();
+        localStorage.removeItem(LS_KEY);
+ 
+     }
+    else {
+        alert('всі поля повинні бути заповнені');
+  }
+    
+}
+
+
+
 
